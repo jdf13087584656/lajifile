088348fa1446ec9ab0f13529a8e5a9417b50e12d
lizewei <12414399@qq.com>
2019-07-13 16:52:22 +0800

    360

--- lvpai/src/main/java/com/xlkj/website/model/UserDto.java
+++ lvpai/src/main/java/com/xlkj/website/model/UserDto.java
@@ -44,23 +44,23 @@ public class UserDto implements Serializable {
     @ApiModelProperty(value = "创建时间", name = "createTime")
     private String createTime;
 
-    public void setUid(Integer uid){this.uid=uid;}
-    public void setAccount(String account){this.account=account;}
-    public void setPassword(String password){this.password=password;}
-    public void setUserName(String userName){this.userName=userName;}
-    public void setCellNumber(String cellNumber){this.cellNumber = cellNumber; }
-    public void setType(Integer type) {this.type = type;}
-    public void setMemo(String memo) {this.memo = memo;}
-    public void setIsDeleted(Integer isDeleted){this.isDeleted = isDeleted;}
-    public void setQuantityCompletion(Integer quantityCompletion) {
-        this.quantityCompletion = quantityCompletion;
-    }
-    public void setUnfinished(Integer unfinished) {
-        this.unfinished = unfinished;
-    }
-    public void setCreateTime(String createTime) {
-        this.createTime = createTime;
-    }
+//    public void setUid(Integer uid){this.uid=uid;}
+//    public void setAccount(String account){this.account=account;}
+//    public void setPassword(String password){this.password=password;}
+//    public void setUserName(String userName){this.userName=userName;}
+//    public void setCellNumber(String cellNumber){this.cellNumber = cellNumber; }
+//    public void setType(Integer type) {this.type = type;}
+//    public void setMemo(String memo) {this.memo = memo;}
+//    public void setIsDeleted(Integer isDeleted){this.isDeleted = isDeleted;}
+//    public void setQuantityCompletion(Integer quantityCompletion) {
+//        this.quantityCompletion = quantityCompletion;
+//    }
+//    public void setUnfinished(Integer unfinished) {
+//        this.unfinished = unfinished;
+//    }
+//    public void setCreateTime(String createTime) {
+//        this.createTime = createTime;
+//    }
     //    public UserDto(Integer uid, String account, String password, String userName, String cellNumber, Integer type, String memo, Integer isDeleted, Integer quantityCompletion, Integer unfinished, String createTime) {
 //        this.uid = uid;
 //        this.account = account;
